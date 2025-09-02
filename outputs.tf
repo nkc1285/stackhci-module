ac3221027e33b05ac8fbf3cb474dba7dff1899d3
nkc1285 <76637254+nkc1285@users.noreply.github.com>
2025-09-01 17:56:52 -0700

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+# Module owners should include the full resource via a 'resource' output
+# https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
+output "resource_id" {
+  description = "This is the full output for the resource."
+  value       = azapi_resource.virtual_machine.id
+}
