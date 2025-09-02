a5c04dca9379d45f359a8530852a0c75c9cdb99e
nkc1285 <76637254+nkc1285@users.noreply.github.com>
2025-09-01 18:02:16 -0700

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,6 +1,6 @@
 terraform {
-  experiments = [module_variable_optional_attrs]
-  required_version = ">= 0.13"
+  required_version = ">= 1.3.0"
+  # Remove the experiments line
   required_providers {
     azapi = {
       source  = "azure/azapi"
